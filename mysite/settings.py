6defb8977caab4e10d69c5cc4dd5a8f5169f67ce
vadim rusu <ruvamd@gmail.com>
2022-06-09 02:18:18 -0700

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",  # new
     "allauth",  # new
     "allauth.account",  # new
     "allauth.socialaccount",  # new
